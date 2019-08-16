89238e755c47f244d8681ce07da2d24614bd6a18
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-08-16 15:25:55 +0100

    Some views for reviews and some route methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,40 @@
 class ReviewsController < ApplicationController
+    before_action :find_review, only: [:edit, :update, :destroy]
+
+    def index
+        @reviews = Review.all
+    end
+
+    def new
+        @review = Review.new
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    # def update
+    #     @review.update
+    #     redirect_to :review_path(@review)
+    # end
+
+    # def destroy
+    #     @review.destroy
+    #     redirect_to :reviews_path
+    # end
+
+    private
+
+    def review_params
+        params.permit(:review).require(:game_id, :user_id, :content, :rating)
+    end
+
+    def find_review
+        @review = Review.find(params[:id])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_08_16_104437) do
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.integer "game_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "platforms", force: :cascade do |t|
+    t.integer "game_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "game_id"
+    t.integer "user_id"
+    t.string "content"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
